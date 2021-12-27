3da91d54fbe0c1b3b432fa4cecbf44407e032ca2
ekaterinalitvinova <ekaterina.litvinova44@gmail.com>
2021-12-27 13:08:43 +0200

    "object" was removed from class, new function was added, type annotation

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,19 +3,27 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class BasePage(object):
+class BasePage:
     def __init__(self, browser):
         self.browser = browser
         self.wait = WebDriverWait(browser, 10)
 
-    def is_element_present(self, how, what):
+    def is_element_present(self, how: str, what: str) -> bool:
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
-            raise ValueError("Can't navigate on the page. Check page elements naming.")
+            raise ValueError(f"Can't navigate on the page. Check page elements naming.")
         return True
 
-    def wait_for_element_present(self, locator):
+    def is_element_not_present(self, how: str, what: str) -> bool:
+        try:
+            element = self.browser.find_element(how, what)
+            if element:
+                raise AssertionError("Element is presented. But shouldn't")
+        except NoSuchElementException:
+            return True
+
+    def wait_for_element_present(self, locator: tuple[str]) -> None:
         try:
             self.wait.until(EC.presence_of_element_located(locator))
         except TimeoutException:
